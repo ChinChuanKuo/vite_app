cb91da471e028290442d4e421b9e217d4400c63b
ChinChuanKuo <chinchuankuo@ChinChuanKuos-MacBook-Pro.local>
2024-01-07 18:03:16 +0800

    update vite_app data

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -3,7 +3,7 @@ import reactLogo from '../../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
 
-function Home() {
+const Home = () => {
   const [count, setCount] = useState(0)
   const countClick = (event) => setCount((count) => count + 1)
 
---
+++ src/views/Home/widgets/head.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+const Head = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Head
